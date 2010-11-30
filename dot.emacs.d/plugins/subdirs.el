2cbefa1d13f3a04ba9dd6cd16a13c1c06b150f08
dot <shuhei.kondo@gmail.com>
2010-11-30 15:20:49 +0900

    add subdirs.el

---
+++ dot.emacs.d/plugins/subdirs.el
@@ -0,0 +1,7 @@
+;; (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+;;     (normal-top-level-add-subdirs-to-load-path))
+
+(add-to-list 'load-path "~/.emacs.d/plugins/rspec-mode")
+(add-to-list 'load-path "~/.emacs.d/plugins/rhtml")
+(add-to-list 'load-path "~/.emacs.d/plugins/rinari")
+(add-to-list 'load-path "~/.emacs.d/plugins/simplenote")
