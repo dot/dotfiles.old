f38d794e9ce4422f3054919e2fd9979be58b80d2
dot <shuhei.kondo@gmail.com>
2012-12-25 14:50:57 +0900

    add ace-jump

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -372,3 +372,24 @@
 (require 'elscreen-howm)
 (require 'elscreen-dired)
 (define-key elscreen-map "\C-]" '(lambda () (interactive) (anything 'anything-c-source-elscreen)))
+
+;; ace-jump
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+;; you can select the key you prefer to
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+;; ;;If you use viper mode :
+;; (define-key viper-vi-global-user-map (kbd "SPC") 'ace-jump-mode)
+;; ;;If you use evil
+;; (define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
