c2cddf3ad0b3af81e944e18bbbf19f608d0b0d26
dot <shuhei.kondo@gmail.com>
2013-07-01 11:10:18 +0900

    update packages

---
+++ dot.emacs.d/elpa/inf-ruby-20130630.2216/inf-ruby-pkg.el
@@ -0,0 +1,1 @@
+(define-package "inf-ruby" "20130630.2216" "Run a ruby process in a buffer [github]" (quote nil))
--- dot.emacs.d/elpa/web-mode-20130623.1753/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20130623.1753" "major mode for editing html templates [github]" (quote nil))
